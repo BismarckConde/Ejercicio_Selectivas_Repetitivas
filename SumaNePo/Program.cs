85dabcb7cb1adbf6bafc853988d1e04fbf0094e8
Sistema2-14 <Sistema2-14@DESKTOP-9OOKO8G>
2024-01-31 15:34:09 -0600

    Agregar archivos de proyecto.

---
+++ SumaNePo/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("\t¡BIENVENIDO AL PROGRAMA DE SUMA DE NUMEROS POSITIVOS Y NEGATIVOS!");
+Console.WriteLine();
+
+Console.WriteLine("Por favor, ingrese 9 numeros enteros.");
+
+int positivos = 0;
+int negativos = 0;
+
+for (int i = 0; i < 9; i++)
+{
+    int num = 0;
+    bool valido = false;
+
+    while (!valido)
+    {
+        Console.Write("Ingrese el numero {0}: ", i + 1);
+        valido = int.TryParse(Console.ReadLine(), out num);
+
+        if (!valido)
+        {
+            Console.WriteLine("¡ERROR! necesita ingresar un numero entero valido...");
+        }
+    }
+    if (num > 0)
+    {
+        positivos += num;
+    }
+    else if (num < 0)
+    {
+        negativos += num;
+    }
+}
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nLa suma de los numeros positivos es: " + positivos);
+Console.WriteLine("\nLa suma de los numeros negativos es: " + negativos);
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\nGracias por usar el programa. Presione cualquier tecla para salir.");
+Console.ReadKey();
---
+++ VentaArt/Program.cs
@@ -0,0 +1,33 @@
+﻿string Codigo;
+int Cantidad;
+double Costo;
+double PagoTotal;
+
+Console.WriteLine(" *** BIENVENIDO AL SISTEMA DE COMPRAS *** ");
+
+Console.WriteLine(" POR FAVOR INGRESE EL CODIGO DE BARRA DEL ARTICULO ");
+Codigo = Console.ReadLine();
+
+Console.WriteLine(" INGRESE LA CANTIDAD DE ARTICULOS ");
+Cantidad = int.Parse(Console.ReadLine());
+
+if (Cantidad ==4)
+{
+    Cantidad = 3;
+    Console.WriteLine(" ¡ ESTAS DE SUERTE!... SOLO SE TE COBRARAN UNICAMENTE 3 ARTICULOS ");
+
+}
+Console.WriteLine(" INGRESA EL COSTO DEL ARTICULO ");
+Costo = Convert.ToDouble(Console.ReadLine());
+
+PagoTotal = Cantidad * Costo;
+if (PagoTotal > 300)
+{
+    int vales = (int)(PagoTotal / 100) * 10;
+    Console.WriteLine(" POR CADA 100 PESOS DE COMPRAS SE TE OBSEQUIARAN "+vales+" PESOS EN VALES  ");
+}
+Console.WriteLine(" EL TOTAL A PAGAR ES : "+PagoTotal);
+Console.WriteLine(" FIN DEL PROGRAMA");
+Console.ReadKey();
+
+
